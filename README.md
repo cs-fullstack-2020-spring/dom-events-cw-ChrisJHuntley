b51168b8c48cc1e5bfaee8a1cd5ea0d9d6233e22
autumn-ragland <raglanda998@gmail.com>
2020-03-03 18:43:18 -0600

    graded

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Event Listener CW
-
+#### YOU MUST COMMENT YOUR FILE (minus one point). Also use more descriptive variable and class names. Put your grid containers inside of a div or section or any kind of tag that supports children tags. Be sure to review grid (you're missing a column at the least). Check out my comments in your JS file
+#### Score : 1/5
 ### Problem 1:
 Create a simple grid system as seen in the attached picture. Put a button called "1" in your main section and another button called "2" in your sidebar section. Put the number 0 in your header. If you click the #1 button the title in your header should change to "1" and the background to blue. If you click the #2 button the title in your header should change to "2" and the background to red. Use classlist to change the background.
 
--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-// sanity cheack
+// sanity check
 console.log('Loaded')
-let changes= document.querySelector('.header')
-let clicked1=document.getElementById('one')
-let clicked2= document.getElementById('two')
-clicked1.addEventListener('click', clicked)
+
+let changes = document.querySelector('.header')
+let clicked1 = document.getElementById('one')
+let clicked2 = document.getElementById('two')
+clicked1.addEventListener('click', clicked) // use a more descriptive function name
+
 function clicked(el) {
-  changes.innerHTML=`${changes.innerHTML} is now 1` 
-  header.classlist.add('coolclass') 
+  changes.innerHTML = `${changes.innerHTML} is now 1` // you should not be appending to the current inner text 
+  header.classlist.add('coolclass') // you have to call your selected element by variable name
 }
-clicked2.addEventListener('click', blicked)
+
+clicked2.addEventListener('click', blicked) // use a more descriptive function name
+
 function blicked(el) {
-  changes.innerHTML=`${changes.innerHTML} is now 2` 
-  header.classlist.add('evencoolerclass') 
+  changes.innerHTML = `${changes.innerHTML} is now 2`// you should not be appending to the current inner text 
+  header.classlist.add('evencoolerclass')// you have to call your selected element by variable name
 }
